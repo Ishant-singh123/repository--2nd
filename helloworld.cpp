246d8991fa52e1ba8d5061947b8ecc210ed5e1a4
Ishant-singh123 <eshusingh114@gmail.com>
2024-10-13 17:06:00 +0530

    improve feature1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello world "<<endl;
+    int age;
+    cout<<"Enter your age "<<endl;
+    cin>>age;
+    if(age>18){
+        cout<<"you are adult "<<endl;
+    }
+    else if(age==18){
+        cout<<"going to adult "<<endl;
+    }
+    else{
+        cout<<"you are boy "<<endl;
+    }
+    
     return 0;
 }
