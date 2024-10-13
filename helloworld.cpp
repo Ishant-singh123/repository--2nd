8dc540b9d12c61ff843d6f4c8e63dc8d1b6f06f0
Ishant-singh123 <eshusingh114@gmail.com>
2024-10-13 16:58:05 +0530

    modify in feature1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
+    int sum(int x, int y){
+        return x+y;
+    }
 int main(){
     cout<<"hello world "<<endl;
+    cout<<"hello GitHub"<<endl;
+    int a,b;
+    cout<<"enter the numbers "<<endl;
+    cin>>a>>b;
+    cout<<"sum is "<<sum(a,b)<<endl;
     return 0;
 }
+
